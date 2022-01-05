4faf44f12a64434f5a5890ac508b2873634af0b9
jocoso <joshua.collado022@gmail.com>
2022-01-05 12:17:51 -0500

    Included the API domain

--- src/components/displayers/contactMeDisplayer.js
+++ src/components/displayers/contactMeDisplayer.js
@@ -31,7 +31,7 @@ class ContactMeDisplayer extends React.Component {
 
         if(this.state.name === '' || this.state.subject === '' || this.state.comment === '') alert('One or more of the input is empty.');
 
-        axios.post('API_URI', this.state).then(
+        axios.post('personal-api-omega.vercel.app', this.state).then(
             res => {
                 this.setState({sent: true}, alert('Message successfully sent.'));
             }).catch(
